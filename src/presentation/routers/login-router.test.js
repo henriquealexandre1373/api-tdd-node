c6b1088afa5521b7c635b74df754da7ceb81096e
Henrique Martins Alexandre <developer.henrique1373@gmail.com>
2023-03-27 16:30:51 -0300

    modify HttpResponse.badRequest to receive an error

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -55,8 +55,9 @@ describe('Login Router', () => {
     }
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-  }
-  )
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+
   test('Should return 500 if no httpRequest is provided', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.route()
